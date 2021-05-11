0c4a4c5e4679b62f5940af149b47c91bd3906721
GregoirePelegrin <pelegrin.gregoire@gmail.com>
2021-05-11 23:28:32 +0200

    More complex sudoku solver

--- solver.py
+++ solver.py
@@ -1,7 +1,17 @@
 data = []
+groups = []
 pool = []
 possibilities = []
 
+def fillPossibilities():
+    for line in data:
+        temp = []
+        for cell in line:
+            if cell == 0:
+                temp.append(pool[:])
+            else:
+                temp.append(cell)
+        possibilities.append(temp)
 def getInitialState(filename):
     with open(filename, "r") as f:
         content = f.read()
@@ -16,15 +26,24 @@ def getInitialState(filename):
             else:
                 temp.append(int(cell))
         data.append(temp)
-def fillPossibilities():
-    for line in data:
-        temp = []
-        for cell in line:
-            if cell == 0:
-                temp.append(pool[:])
-            else:
-                temp.append(cell)
-        possibilities.append(temp)
+def groupGeneration():
+    groups.clear()
+    for i in range(len(pool)):
+        line = []
+        col = []
+        for j in range(len(pool)):
+            line.append([i, j])
+            col.append([j, i])
+        groups.append(line)
+        groups.append(col)
+    size = int(len(pool) ** 0.5)
+    for i in range(size):
+        for j in range(size):
+            block = []
+            for k in range(size):
+                for l in range(size):
+                    block.append([i*size+k, j*size+l])
+            groups.append(block)
 def poolGeneration():
     for i in range(1, len(data)+1):
         pool.append(i)
@@ -33,55 +52,60 @@ def solve():
     while not solved:
         updatePossibilities()
         updateData()
+        print("Current data: {}".format(data))
         solved = True
         for line in data:
             if 0 in line:
                 solved = False
                 break
+        if input("Continue? (y/n) ") == "n":
+            break
 def updateData():
-    for i in range(len(data)):
-        for j in range(len(data[i])):
+    for i in range(len(pool)):
+        for j in range(len(pool)):
             if type(possibilities[i][j]) != int and len(possibilities[i][j]) == 1:
                 data[i][j] = possibilities[i][j][0]
 def updatePossibilities():
-    for i in range(len(data)):
-        for j in range(len(data)):
-            verifCell(i, j)
-def verifCell(i, j):
-    if type(possibilities[i][j]) != int:
-        for cell in data[i]:
-            try:
-                possibilities[i][j].remove(cell)
-            except:
-                pass
-
-        for line in data:
-            try:
-                possibilities[i][j].remove(line[j])
-            except:
-                pass
-        
-        size = int(len(data)**0.5)
-        xMin = int(i / size) * size
-        xMax = xMin + size
-        yMin = int(j / size) * size
-        yMax = yMin + size
-
-        for x in range(xMin, xMax):
-            for y in range(yMin, yMax):
-                if x == i and y == j:
-                    continue
+    for group in groups:
+        verifGroup(group)
+def verifGroup(g):
+    for i in range(len(g)):
+        content = possibilities[g[i][0]][g[i][1]]
+        if type(content) == int:
+            for j in range(len(g)):
                 try:
-                    possibilities[i][j].remove(data[x][y])
+                    possibilities[g[j][0]][g[j][1]].remove(content)
                 except:
                     pass
+            continue
+        count = 1
+        for j in range(len(g)):
+            if i != j and content == possibilities[g[j][0]][g[j][1]]: 
+                count += 1
+        if len(content) == count:
+            for j in range(len(g)):
+                if content != possibilities[g[j][0]][g[j][1]]:
+                    for c in content:
+                        try:
+                            possibilities[g[j][0]][g[j][1]].remove(c)
+                        except:
+                            pass
 
 getInitialState("data.txt")
 poolGeneration()
-# print("Data: {}\nPool: {}".format(data, pool))
+groupGeneration()
 
 fillPossibilities()
-# print("Possibilities: {}".format(possibilities))
 
 solve()
-print("Data: {}".format(data))
+
+soluce = ""
+size = int(len(pool) ** 0.5)
+for i,line in enumerate(data):
+    if i != 0 and i % size == 0: soluce += "=======================\n"
+    for j,cell in enumerate(line):
+        if j != 0 and j % size == 0: soluce += "|| "
+        soluce += "{}".format(cell)
+        if j != len(line)-1: soluce += " "
+    if i != len(data)-1: soluce += "\n"
+print(soluce)
